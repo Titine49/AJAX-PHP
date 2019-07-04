58eb1f6ba979e293ac6759d963c1b62581dcd3d2
Titine49 <51953026+Titine49@users.noreply.github.com>
2019-07-04 10:09:08 +0200

    Modif footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,8 @@
 <div class="ui container" id="list" style="padding: 8em 0;">
 
+    
 </div>
 
+<footer id="footer"></footer>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,15 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
     <script src="js/script.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        #footer {
+            background-color: grey; 
+            width: 100%;
+            height: 90px;
+        }
+
+</style>
     <title>Trouvez votre FoodTruck !</title>
 </head>
 <body>
