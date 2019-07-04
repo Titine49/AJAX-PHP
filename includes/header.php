95f4a3e9e3ac4ba03bdebb83f3441853d29b108f
Titine49 <51953026+Titine49@users.noreply.github.com>
2019-07-04 11:16:59 +0200

    V2 footer

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
     <script src="js/script.js"></script>
+    <link rel="stylesheet" href="css/style.css">
     <title>Trouvez votre FoodTruck !</title>
 </head>
 <body>
